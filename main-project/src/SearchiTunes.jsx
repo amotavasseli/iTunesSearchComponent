8d4a189733e251b8bd9a08f91f7cbadc793df7a4
Arian Motavasseli <amotavasseli@gmail.com>
2018-03-15 15:36:26 -0700

    completed

--- main-project/src/SearchiTunes.jsx
+++ main-project/src/SearchiTunes.jsx
@@ -13,69 +13,72 @@ class Search extends React.Component {
     findAlbums = () => {
         const artistName = encodeURIComponent(this.state.artistName);
         const url = `https://itunes.apple.com/search?term=${artistName}`;
-        console.log(url);
         axios.get(url)
             .then(
-                response => {
-                    const results = response.data.results;
-                    console.log(results);
-                    let artists = {};
-                    for (const item of results) {
-                        if (artists.artistId !== item.artistName)
-                            artists[item.artistId] = item.artistName;
-                    }
-                    artists = Object.entries(artists);
-                    this.setState({ artists: artists, albums: null });
-                    console.log(artists);
-                },
-                error => alert(error)
+            response => {
+                const results = response.data.results;
+                let artists = {};
+                for (const item of results) {
+                    if (artists.artistId !== item.artistName)
+                        artists[item.artistId] = item.artistName;
+                }
+                artists = Object.entries(artists);
+                this.setState({ artists: artists, albums: null });
+            },
+            error => this.setState({error: 'could not retrieve artist info'})
             )
-
     }
     showAlbums = artist => {
         const artistId = encodeURIComponent(artist[0]);
         const url = `https://itunes.apple.com/lookup?id=${artistId}&entity=album`;
         axios.get(url)
             .then(
-                response => {
-                    console.log(response);
-                    this.setState({ albums: response.data.results, artists: null })
-                },
-                error => alert(error)
+            response => {
+                this.setState({ albums: response.data.results, artists: null })
+            },
+            error =>  this.setState({error: 'could not retrieve album info'})
             )
     }
-
+    enterPressed = key => {
+        if (key.charCode === 13) {
+            this.findAlbums();
+        }
+    }
     render() {
         return (
             <div>
-                <div>
+
+                <div className="search-container">
                     <label>
                         <span className='label'>Search by Artist</span>
                         <br />
-                        <input 
-                            type='text' 
-                            placeholder='Enter Artist Name' 
-                            value={this.state.artistName} 
-                            onChange={this.artistInput} 
+                        <input
+                            type='text'
+                            placeholder='Enter Artist Name'
+                            value={this.state.artistName}
+                            onChange={this.artistInput}
+                            onKeyPress={this.enterPressed}
                         />
                     </label>
-                    <button type='button' onClick={this.findAlbums}>Search</button>
+                    <button type='button' onClick={this.findAlbums}>
+                        <i className="fa fa-search"></i>
+                    </button>
                 </div>
-                <div className='artist'>
+                <span style={{color: 'red'}}>{this.state.error}</span>
+                <div className='artists'>
                     {
-                        this.state.artists  &&
+                        this.state.artists &&
                         <div>
                             {
                                 this.state.artists.map(([artistId, artistName], index) => (
-                                    <div key={artistId}>
-                                        <a 
-                                            href="javascript:;" 
-                                            onClick={() => this.showAlbums([artistId, artistName])}
-                                        >
-                                            {artistName}
-                                        </a>
-                                    </div>
 
+                                    <a
+                                        key={artistId} className='artist'
+                                        href="javascript:;"
+                                        onClick={() => this.showAlbums([artistId, artistName])}
+                                    >
+                                        {artistName}
+                                    </a>
                                 ))
                             }
 
@@ -87,23 +90,21 @@ class Search extends React.Component {
                         this.state.albums &&
                         this.state.albums.map((album, index) =>
                             album.collectionId &&
-                            <div key={index} className='album'>
-                                <img 
-                                    src={album.artworkUrl100} 
-                                    alt={album.collectionName}
-                                />
-                                <a 
-                                    href={album.collectionViewUrl}
-                                >
+                            <a key={index}
+                                href={album.collectionViewUrl}
+                            >
+                                <div className='album'>
+                                    <img
+                                        src={album.artworkUrl100}
+                                        alt={album.collectionName}
+                                    />
                                     {album.collectionName}
-                                </a>
-
-                            </div>
+                                </div>
+                            </a>
                         )
                     }
                 </div>
             </div>
-
         )
     }
 }
